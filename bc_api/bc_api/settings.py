084e4036748dcedb1487a30e7f9b5cca88daa873
Marija Cehic <cehicmarija@gmail.com>
2021-03-28 01:01:06 +0100

    resolved privacy concerns

--- bc_api/bc_api/settings.py
+++ bc_api/bc_api/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@wu9(01qrywop#xsz7ro0q3!&x+s)r_war2d@!8v6eks1c4ga2'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
