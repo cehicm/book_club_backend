3f1ad8878a980ec2195af45977af44f2959f40ef
Marija Cehic <cehicmarija@gmail.com>
2021-04-30 14:55:06 +0200

    added views to user and review

--- bc_api/apps/review/views.py
+++ bc_api/apps/review/views.py
@@ -2,11 +2,29 @@ from django.shortcuts import render
 from django.views.generic import View
 from apps.review.models import Review
 from django.core import serializers
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
+
+@method_decorator(csrf_exempt, name='dispatch')
 
 class ReviewListView(View):
     def get(self, request, *args, **kwargs):
         review_queryset = Review.objects.all()
         review_serializer = serializers.serialize('json', review_queryset)
         return HttpResponse(review_serializer, "application/json")
+
+    def post(self,request):
+        json_data = json.loads(request.body)
+        from = ReviewForm(json_data)
+
+        if form.is_valid():
+            book = json_data("book") #?
+            user = json_data("user")
+            comment = json_data("comment")
+            score = json_data("score")
+            return JsonResponse(book,user,comment,score, status=200, safe=False)
+        
+        else:
+            return JsonResponse("Form Invalid", status=400, safe=False)
--- bc_api/apps/user/views.py
+++ bc_api/apps/user/views.py
@@ -3,11 +3,26 @@ from django.shortcuts import render
 from django.views.generic import View
 from apps.user.models import BCUser
 from django.core import serializers
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
 
+@method_decorator(csrf_exempt, name='dispatch')
 class BCUserListView(View):
     def get(self, request, *args, **kwargs):
         bcuser_queryset = BCUser.objects.all()
         bcuser_serialized = serializers.serialize('json', bcuser_queryset)
         return HttpResponse(bcuser_serialized, content_type="application/json")
+
+    def post(self,request):
+        json_data = json.loads(request.body)
+
+        form = BookFrom(json_data)
+
+        if form.is_valid():
+            user = json_data["user"]
+            return JsonResponse(user, status=200, safe=False)
+        else:
+            return JsonResponse("Form Invalid", status=400, safe=False)
